96c7edbf80c1ca5397c76a2e739441fa9a73e888
Aish-dindukurthi01 <aishudindukurthi@gmail.com>
2021-02-07 18:13:40 +0530

    Create nubers.py

---
+++ nubers.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+ d=(a+b)*c
+if d>100
+ print("number is greater than 100",d)
+else:
+ print("number is less than 100",d)
